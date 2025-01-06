f7c1140a59391976c0584a90ce5cdf20fd6cce70
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2025-01-06 18:19:12 +0300

    WorkerService

---
+++ RabbitMQ.MassTransit.WorkerService.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.MassTransit.WorkerService.Publisher.Services;
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddMassTransit(configurator =>
+        {
+            configurator.UsingRabbitMq((context, _configurator) =>
+            {
+                _configurator.Host("");
+            }
+            );
+        });
+
+        services.AddHostedService<PublishMessageService>(provider =>
+        {
+            using IServiceScope scope = provider.CreateScope();
+            IPublishEndpoint publishEndpoint = scope.ServiceProvider.GetService<IPublishEndpoint>();
+            return new(publishEndpoint);
+        });
+    })
+    .Build();
+
+await host.RunAsync();
