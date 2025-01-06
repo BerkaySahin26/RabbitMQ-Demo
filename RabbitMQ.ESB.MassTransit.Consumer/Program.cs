1f75db0686a2ca12dd6caa348278dd22b24ff4b3
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2025-01-06 17:50:14 +0300

    MassTransit

---
+++ RabbitMQ.ESB.MassTransit.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.Consumer.Consumers;
+
+string rabbitMQUri = "";
+
+string queueName = "example-queue";
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq
+    (factory =>
+    {
+        factory.Host(rabbitMQUri);
+        factory.ReceiveEndpoint(queueName,endpoint =>
+         {
+             endpoint.Consumer<ExampleMessageConsumer>();
+         
+         });
+    });
+
+await bus.StartAsync();
+
+Console.Read();
---
+++ RabbitMQ.ESB.MassTransit.Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.Shared.Messages;
+string rabbitMQUri = "";
+
+string queueName = "example-queue";
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq
+    (factory =>
+    {
+        factory.Host(rabbitMQUri);
+    });
+
+ISendEndpoint sendEndpoint = await bus.GetSendEndpoint(new($"{rabbitMQUri}/{queueName}"));
+
+Console.Write("Gönderlicek mesaj :");
+string message = Console.ReadLine();
+await sendEndpoint.Send<IMessage>(new ExampleMessage()
+{
+    Text = message 
+});
+
+Console.Read();
