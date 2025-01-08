d09719bb78eef3635394a6c2af038e507fed457c
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2025-01-08 20:22:25 +0300

    MassTransit & Request Response pattern

---
+++ RabbitMQ.ESB.MassTransit.RequestResponse.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.RequestResponse.Consumer.Consumer;
+
+
+Console.WriteLine("Consumer");
+
+string rabbitMQUri = "";
+
+string requestQueue = "request-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+
+    factory.ReceiveEndpoint(requestQueue, endpoint =>
+    {
+        endpoint.Consumer<RequestMessageConsumer>();
+    });
+});
+
+await bus.StartAsync();
+
+Console.Read();
--- RabbitMQ.MassTransit.WorkerService.Consumer/Program.cs
+++ RabbitMQ.MassTransit.WorkerService.Consumer/Program.cs
@@ -12,7 +12,7 @@ IHost host = Host.CreateDefaultBuilder(args)
 
             configurator.UsingRabbitMq((context, _configurator) =>
             {
-                _configurator.Host("amqps://befjdvjy:bs5zD-4j8OfHQrZFUOnEAKomCudYmkL1@moose.rmq.cloudamqp.com/befjdvjy");
+                _configurator.Host("");
 
                 _configurator.ReceiveEndpoint("example-message-queue", e => e.ConfigureConsumer<ExampleMessageConsumer>(context));
             });
